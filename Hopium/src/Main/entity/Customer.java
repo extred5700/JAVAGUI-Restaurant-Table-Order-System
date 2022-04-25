772c49c4e8e93fa00dc7b62a78401f234bcb1051
TTW <77585757+extred5700@users.noreply.github.com>
2022-04-25 21:02:55 +0800

    Add files via upload

---
+++ Hopium/src/Main/entity/Customer.java
@@ -0,0 +1,37 @@
+package Main.entity;
+
+public class Customer {
+    // Variable declaration
+    String pNum; // Phone Number
+    int table_num; // Table Number
+
+    // Function to display Menu in CustomerPageUI.java
+    public void viewMenu(){
+
+    }
+
+    // Function to display Cart in CustomerPageUI.java
+    public void viewCart(){
+
+    }
+
+    // Function to add items to the cart & DB
+    public void addToCart(String item_id, String name, int qty){
+
+    }
+
+    // Function to remove items from the cart & DB
+    public void removeFromCart(String item_id, String name, int qty){
+
+    }
+
+    // Function to search for items from the menu
+    public void searchItem(String name){
+
+    }
+
+    // Function to redirect user to the payment menu
+    public void makePayment(int table_num, String p_num){
+
+    }
+}
