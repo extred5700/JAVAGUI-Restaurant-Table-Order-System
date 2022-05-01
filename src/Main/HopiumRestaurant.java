ea667c482ce2439a615c533ac0d490d49e498ac4
TTW <77585757+extred5700@users.noreply.github.com>
2022-05-01 22:40:19 +0800

    Add files via upload

--- src/Main/HopiumRestaurant.java
+++ src/Main/HopiumRestaurant.java
@@ -1,14 +1,18 @@
 package Main;
 
 import Main.boundary.*;
+import Main.boundary.StaffUI.ManagerPageUI;
+import Main.boundary.StaffUI.OwnerPageUI;
 
 public class HopiumRestaurant {
     public static void main(String[] args) {
-        new StartingPage();
+        //new StartingPage();
 
         /* Staff */
         //new StaffLoginPage();
         //new UserAdminPageUI("ADMIN TEST");
+        new OwnerPageUI("Owner Test");
+        //new ManagerPageUI("Manager Test");
 
         /* Customer */
         //new CustomerLoginPage();
