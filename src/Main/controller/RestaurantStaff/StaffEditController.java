2cd5e3bfd0bcc9d4a05f850a197e3a95acac47a0
TTW <77585757+extred5700@users.noreply.github.com>
2022-05-02 19:31:15 +0800

    Add files via upload

---
+++ src/Main/controller/RestaurantStaff/StaffEditController.java
@@ -0,0 +1,5 @@
+package Main.controller.RestaurantStaff;
+
+public class StaffEditController{
+
+}
---
+++ src/Main/controller/RestaurantStaff/StaffViewController.java
@@ -0,0 +1,14 @@
+package Main.controller.RestaurantStaff;
+
+import Main.entity.Order;
+import Main.entity.Restaurant_Staff;
+
+import java.util.ArrayList;
+
+public class StaffViewController {
+    Restaurant_Staff restaurant_staff = new Restaurant_Staff();
+    Order order = new Order();
+    public ArrayList<ArrayList<String>> displayOrders(){
+        return restaurant_staff.viewOrders();
+    }
+}
