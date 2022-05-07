7193e4e6f96da5e71f72abf202a5f73990c932ea
TTW <77585757+extred5700@users.noreply.github.com>
2022-05-07 18:15:15 +0800

    Add files via upload

--- src/Main/controller/UserAdmin/SuspendUserController.java
+++ src/Main/controller/UserAdmin/SuspendUserController.java
@@ -1,10 +1,13 @@
 package Main.controller.UserAdmin;
 
-import Main.entity.Staff;
 import Main.entity.User_Admin;
 
 public class SuspendUserController{
     User_Admin userAdmin = new User_Admin();
+
+    public String [][] getUserInfo(){
+        return userAdmin.getUserInfoFromDB();
+    }
 
     public boolean suspendUserAccount(String selectedUsername, String newActiveStatus){
         return userAdmin.suspendAccount(selectedUsername, newActiveStatus);
