09f7bdf68b6036811a15d618867a838a4028ae58
kayqueue <104192530+kayqueue@users.noreply.github.com>
2022-05-10 22:49:05 +0800

    Delete ViewCartController.java

--- src/Main/controller/Customer/ViewCartController.java
+++
@@ -1,9 +0,0 @@
-package Main.controller.Customer;
-
-import Main.entity.Customer;
-
-public class ViewCartController {
-    public String [][] viewCart(Customer customer) {
-        return customer.viewCart();
-    }
-}
