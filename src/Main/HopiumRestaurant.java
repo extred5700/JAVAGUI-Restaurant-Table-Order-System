40a7b72b579590743a1880ea917d811cd66adfe1
TTW <tzeweitan@outlook.com>
2022-05-28 16:11:11 +0800

    Main method edit

--- src/Main/HopiumRestaurant.java
+++ src/Main/HopiumRestaurant.java
@@ -1,27 +1,23 @@
 package Main;
 
 import Main.boundary.CustomerLoginPage;
+import Main.boundary.CustomerUI.CustomerPageUI;
+import Main.boundary.StaffLoginPage;
 import Main.boundary.StaffUI.*;
 import Main.boundary.StartingPage;
 
 public class HopiumRestaurant {
     public static void main(String[] args) {
-        //new StartingPage();
+        new StartingPage();             // Landing page of program 
 
-        /* Staff */
-        //new StaffLoginPage();
-        // Admin
-        //new UserAdminPageUI();
-        // Owner
-        //new OwnerPageUI();
-        // Manager
-        new ManagerPageUI();
-        // Restaurant Staff
-        //new RestaurantStaffPageUI();
+        /* Restaurant Actors */
+        //new StaffLoginPage();         // Restaurant Actors login page
+        //new UserAdminPageUI();        // User Admin Homepage UI
+        //new OwnerPageUI();            // Restaurant Owner Homepage UI
+        //new ManagerPageUI();          // Restaurant Manager Homepage UI
+        //new RestaurantStaffPageUI();  // Restaurant Staff Homepage UI
 
         /* Customer */
-        //new CustomerLoginPage();
-        //new CustomerPageUI(42);
-
+        //new CustomerLoginPage();      // Customer select Table Number UI
     }
 }
